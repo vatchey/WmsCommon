d60e2cc8d34669ee3e3536823eb35980f258dc5e
vatche <vatchey@gmail.com>
2025-02-19 09:36:56 -0800

    wip - added MeasurementUnit type

---
+++ Enums/MeasurementUnit.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+
+namespace WmsCommon.Enums
+{
+	// ensure it matches the database table and plc enum
+	public enum MeasurementUnit : short
+	{
+		[Description("Undefined")]
+		Undefined = 0,
+
+		[Description("mm")]
+		Millimeter,
+		[Description("V AC")]
+		AcVoltage,
+		[Description("A AC")]
+		AcCurrent,
+		[Description("psi")]
+		Psi
+	}
+}
